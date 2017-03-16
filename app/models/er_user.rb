d8a13c9f802dd5ed3283a3661e159b13629ba136
lovesinghsaroha <lovesinghsaroha@gmail.com>
2017-03-16 13:21:11 +0530

    Update er_user.rb

--- app/models/er_user.rb
+++ app/models/er_user.rb
@@ -1,6 +1,6 @@
 class ErUser < ApplicationRecord
 	has_secure_password
-	validates_presence_of :email , :name
+	validates_presence_of :email
 	validates_confirmation_of :password
 	validates_uniqueness_of :email
 end
