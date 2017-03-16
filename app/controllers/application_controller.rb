4a964a85a2ccac947d08b2359e68be7fe458ab56
lovesinghsaroha <lovesinghsaroha@gmail.com>
2017-03-16 15:00:25 +0530

    Committed via Octokit!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,17 @@
 class ApplicationController < ActionController::Base
-\n  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 
-  \n
+  
   def islogged
-\n  	if not session[:er_usr_parm]
- \n 		redirect_to "/welcome"
- \n 	end
-\n  end
+  	if not session[:er_usr_parm]
+  		redirect_to "/welcome"
+  	end
+  end
 
- \n def isloggedj
- \n 	if not session[:er_usr_parm]
- \n 		render json: {error: "You are not logged in.."} , status: 500
- \n 	end
- \n end
-\n end
+  def isloggedj
+  	if not session[:er_usr_parm]
+  		render json: {error: "You are not logged in.."} , status: 500
+  	end
+  end
+end
+
